ea13ccb5582782b402d03dc3ddeb93cea923e6c0
agita-090909 <62494052+agita-090909@users.noreply.github.com>
2020-04-24 14:11:44 +0700

    Add files via upload

---
+++ db.php
@@ -0,0 +1,25 @@
+<?php
+echo "<br>";
+echo "<p align=center>";
+echo "<font face='times new roman' size=5>DATA PEMANTAUAN COVID-19 WILAYAH BANTEN</font>";
+echo "<br>";
+echo "<font face='times new roman' size=5>Per tanggal " . date("d/m/Y ") . "- " . date("h:i:sa") . "</font>";
+echo "<br>";
+echo "<br><font face='times new roman' size=5>AGITA SWARA RAMADHAN / 2016142121</font>";
+echo "<br>";
+echo "<br>";
+echo "<table width = 1000 border =1>
+<tr>
+<td><p align=center>POSITIF</td>
+<td><p align=center>DIRAWAT</td>
+<td><p align=center>SEMBUH</td>
+<td><p align=center>MENINGGAL</td>
+</tr>
+<tr>
+<td><p align=center>222</td>
+<td><p align=center>159</td>
+<td><p align=center>27</td>
+<td><p align=center>36</td>
+</tr>
+</table>";
+?>
